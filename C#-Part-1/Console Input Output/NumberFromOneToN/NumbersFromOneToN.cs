ed0288deea163fd3aa99b8380123cec0ec8bd19e
Warez888 <vladimir.dimitrov94@outlook.com>
2016-04-14 18:15:48 +0300

    Console In-Out

---
+++ C#-Part-1/Console Input Output/NumberFromOneToN/NumbersFromOneToN.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class NumbersFromOneToN
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        if (1 <= n && n <= 1000)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+
---
+++ C#-Part-1/Console Input Output/QuadraticEquation/QuadraticEquation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class QuadraticEquation
+{
+    static void Main()
+    {
+        double a = double.Parse(Console.ReadLine());
+        double b = double.Parse(Console.ReadLine());
+        double c = double.Parse(Console.ReadLine());
+        double discriminant = Math.Sqrt(Math.Pow(b, 2) - (4 * a * c));
+        if (discriminant > 0)
+        {
+            double rootOne = (-b + discriminant) / (2 * a);
+            double rootTwo = (-b - discriminant) / (2 * a);
+            Console.WriteLine("{0:0.00}", Math.Min(rootOne, rootTwo));
+            Console.WriteLine("{0:0.00}", Math.Max(rootOne, rootTwo));
+        }
+        else if (discriminant == 0)
+        {
+            double root = -b / (2 * a);
+            Console.WriteLine("{0:0.00}", root);
+        }
+        else
+        {
+            Console.WriteLine("no real roots");
+        }
+    }
+}
+
