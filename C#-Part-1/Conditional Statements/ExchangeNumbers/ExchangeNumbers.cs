2a989f721a6d741f9b41cf40cb513819e1359ab6
Warez888 <vladimir.dimitrov94@outlook.com>
2016-04-14 18:29:27 +0300

    Completed tasks

---
+++ C#-Part-1/Conditional Statements/ExchangeNumbers/ExchangeNumbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class ExchangeNumbers
+{
+    static void Main()
+    {
+        double a = double.Parse(Console.ReadLine());
+        double b = double.Parse(Console.ReadLine());
+        double min, max;
+        if (a > b)
+        {
+            min = b;
+            max = a;
+            Console.WriteLine("{0} {1}", min, max);
+        }
+        else
+        {
+            min = a;
+            max = b;
+            Console.WriteLine("{0} {1}", min, max);
+        }
+    }
+}
+
