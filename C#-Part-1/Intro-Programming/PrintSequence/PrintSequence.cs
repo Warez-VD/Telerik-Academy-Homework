502e7a056244c669e1fb5d8b2d78fe90490f342f
Warez888 <vladimir.dimitrov94@outlook.com>
2016-04-14 18:04:46 +0300

    Completed tasks

---
+++ C#-Part-1/Intro-Programming/PrintSequence/PrintSequence.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Print_Sequence
+{
+    class PrintSequence
+    {
+        static void Main()
+        {
+            for (int i = 2; i < 12; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine("{0}", i);
+                }
+                else
+                {
+                    Console.WriteLine("-{0}", i);
+                }
+            }
+        }
+    }
+}
---
+++ C#-Part-1/Intro-Programming/SquareRoot/SquareRoot.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Square_root
+{
+    class SquareRoot
+    {
+        static void Main()
+        {
+            double number = Math.Sqrt(12345);
+            Console.WriteLine(number);
+        }
+    }
+}
