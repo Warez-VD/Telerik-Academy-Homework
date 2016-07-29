b6b03471587020843bfeca1a2d982f35fa723023
Warez888 <vladimir.dimitrov94@outlook.com>
2016-07-29 23:25:07 +0300

    Exception handling

---
+++ C# Part 2/Exception Handling/Enter number/EnterNumber.cs
@@ -0,0 +1,54 @@
+﻿namespace EnterNumber
+{
+    using System;
+    using System.Text;
+
+    class EnterNumber
+    {
+        static int ReadNumber(int start, int end)
+        {
+            int number = int.Parse(Console.ReadLine());
+            if (start > number && number > end)
+            {
+                throw new FormatException();
+            }
+            return number;
+        }
+
+        static void Main()
+        {
+            int[] numbers = new int[12];
+            numbers[0] = 1;
+            numbers[11] = 100;
+            var result = new StringBuilder();
+
+            try
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    numbers[i] = ReadNumber(1, 100);   
+                }
+
+                for (int i = 1; i < numbers.Length; i++)
+                {
+                    if (numbers[i] <= numbers[i - 1])
+                    {
+                        throw new FormatException();
+                    }
+                }
+
+                for (int i = 0; i < numbers.Length - 1; i++)
+                {
+                    result.Append(numbers[i] + " < ");
+                }
+                result.Append(numbers[numbers.Length - 1]);
+                Console.WriteLine(result);
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("Exception");
+            }
+            
+        }
+    }
+}
---
+++ C# Part 2/Exception Handling/Square root/SquareRoot.cs
@@ -0,0 +1,40 @@
+﻿namespace SquareRoot
+{
+    using System;
+
+    class SquareRoot
+    {
+        static double SquareR(double num)
+        {
+            double result = 0;
+
+            if (num < 0)
+            {
+                throw new FormatException();
+            }
+            else
+            {
+                result = Math.Sqrt(num);
+            }
+
+            return result;
+        }
+
+        static void Main()
+        {
+            try
+            {
+                double number = double.Parse(Console.ReadLine());
+                Console.WriteLine("{0:F3}", SquareR(number));
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
+        }
+    }
+}
