fda8cd907fdff0cfa40c333a496a9a4a73078386
Vladimir Dimitrov <vlado547@outlook.com>
2016-08-26 23:14:21 +0300

    Update task-2.js

--- JavaScript UI&DOM/jQuery Overview/task-2.js
+++ JavaScript UI&DOM/jQuery Overview/task-2.js
@@ -9,31 +9,29 @@ function HideOrShowContent(selector) {
     $element = $(selector);
 
     $element.children(".button")
-                .html("hide")
-                .on("click", function () {
-                    var $currentElement = $(this);
+            .html("hide")
+            .on("click", function () {
+                var $currentElement = $(this);
 
-                    if ($currentElement.next().hasClass("button")) {
+                if ($currentElement.next().hasClass("button")) {
+                    return;
+                }
+                    
+                while ($currentElement) {
+                    if($currentElement.next().hasClass("content") &&
+                        $currentElement.next().css("display") === 'none') {
+                        $currentElement.next().css("display", "");
+                        $currentElement.prev(".button").html("hide");
+                        return;
+                    } else if ($currentElement.next().hasClass("content")) {
+                        $currentElement.next().css("display", "none");
+                        $currentElement.prev(".button").html("show");
                         return;
                     }
-                    
-                    while ($currentElement) {
-                        if($currentElement.next().hasClass("content") &&
-                            $currentElement.next().css("display") === 'none') {
-                            $currentElement.next().css("display", "");
-                            $currentElement.prev(".button").html("hide");
-                            return;
-                        } else if ($currentElement.next().hasClass("content")) {
-                            $currentElement.next().css("display", "none");
-                            $currentElement.prev(".button").html("show");
-                            return;
-                        }
 
-                        $currentElement = $currentElement.next();
-                    }
-                });
-    
-
+                    $currentElement = $currentElement.next();
+                }
+    });
 }
 
 HideOrShowContent("#root");
